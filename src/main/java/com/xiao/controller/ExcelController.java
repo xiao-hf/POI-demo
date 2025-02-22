01c8153882b6b578174dade7cf3d682c82425cfc
Administrator <763820835@qq.com>
2025-02-22 20:59:48 +0800

    下载文件到浏览器

---
+++ src/main/java/com/xiao/controller/ExcelController.java
@@ -0,0 +1,32 @@
+package com.xiao.controller;
+
+import com.xiao.domain.TpSysUser;
+import com.xiao.mapper.TpSysUserMapper;
+import com.xiao.utils.PoiUtil;
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("excel")
+public class ExcelController {
+    @Resource
+    TpSysUserMapper tpSysUserMapper;
+    @GetMapping("export")
+    public String export(HttpServletResponse resp) throws IOException {
+        List<TpSysUser> users = tpSysUserMapper.selectOrderByExpireTimeDesc();
+        ServletOutputStream os = resp.getOutputStream();
+        String fileName = "users.xlsx";
+        resp.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+        PoiUtil.writeListToOutputStream(os, users, TpSysUser.class);
+        os.flush();
+        os.close();
+        return "导出成功!";
+    }
+}
